470999e02fed3a635bca55113dee329af87cb54f
akshatadsule <akshat.adsule@gmail.com>
2020-07-28 15:00:53 -0700

    End of lesson 283

--- app.js
+++ app.js
@@ -3,15 +3,35 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    var today = new Date();
-
-    if(today.getDay() == 6 || today.getDay() == 0) {
-        res.send("<h1>It's the weekend!</h1>");
-    } else {
-        res.sendFile(__dirname + "/index.html")
+    let today = new Date();
+    let day = "";
+    switch(today.getDay()){
+        case 0: 
+            day = "Sunday";
+            break;
+        case 1: 
+            day = "Monday";
+            break;
+        case 2: 
+            day = "Tuesday";
+            break;
+        case 3: 
+            day = "Wednesday";
+            break;
+        case 4: 
+            day = "Thursday";
+            break;
+        case 5: 
+            day = "Friday";
+            break;
+        case 6: 
+            day = "Saturday";
+            break;
     }
+    res.render("list", {kindOfDay: day});
 });
 
 app.listen(7000, function() {
