173fc89dd8d89b7e08497fbce7129fa9f66b6726
akshatadsule <akshat.adsule@gmail.com>
2020-07-28 13:16:57 -0700

    End of lesson 282

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const express = require('express');
-const 
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function (req, res) {
+    var today = new Date();
+
+    if(today.getDay() == 2 || today.getDay() == 0) {
+        res.send("<h1>It's the weekend!</h1>");
+    } else {
+        res.send("<h1>I have to work.</h1>");
+    }
+});
+
+app.listen(7000, function() {
+    console.log("Server is running on port 7000");
+});
